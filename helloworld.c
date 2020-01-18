8dd4d60f62c0cfaf2ff754039723e32b78e86f89
mahendra-mehra <mahendra488@gmail.com>
2020-01-18 18:19:56 +0530

    commit from new machine by new user

--- helloworld.c
+++ helloworld.c
@@ -2,5 +2,6 @@
     int main() {
        // printf() displays the string inside quotation
        printf("Hello, welcome to first lab of Devops! by Prof.Mahendra Mehra");
+       printf("This lab is on Distributed Version Control System: GIT");
        return 0;
     }
